916e15007beb2a56328cecd2b604d04ed5bef9ed
Allison Zhang <alzhang19@ucla.edu>
2022-01-13 22:24:00 -0800

    Add page components (#52)
    * Add page components
    
    Created empty page components and added them as routes
    
    * fix lint issues with page components
    
    * Removed English page

---
+++ src/components/pages/HungerIfElse.tsx
@@ -0,0 +1,9 @@
+function HungerIfElse(): JSX.Element {
+  return (
+    <div>
+      <div>If-else statement page 1: Hunger</div>
+    </div>
+  );
+}
+
+export default HungerIfElse;
---
+++ src/components/pages/Nested.tsx
@@ -0,0 +1,9 @@
+function Nested(): JSX.Element {
+  return (
+    <div>
+      <div>Nested Statements page</div>
+    </div>
+  );
+}
+
+export default Nested;
