ad214b88531b6c3057a3bae968f95d513b0e8f4d
Luke <77371718+ldalton02@users.noreply.github.com>
2022-01-31 15:43:13 -0800

    * Adds react-rewards package to have confetti pop up upon the modal   opening * Allows the modal to be opened by the open prop that is passed in   changing

--- src/components/pages/Landing.tsx
+++ src/components/pages/Landing.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PlayAgain from '../shared/PlayAgain';
 
 export default function Landing(): JSX.Element {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setModalOpen(true);
+    }, 3000);
+  }, []);
 
   return (
     <React.Fragment>
-      <PlayAgain>
+      <PlayAgain
+        modalOpen={modalOpen}
+      >
 
       </PlayAgain>
     </React.Fragment>
--- src/components/shared/PlayAgain.tsx
+++ src/components/shared/PlayAgain.tsx
@@ -1,18 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Modal from 'react-modal';
 import '../../styles/playAgain.scss';
+import Reward, { RewardElement } from 'react-rewards';
 import ChoiceButton from './ChoiceButton';
 
-/*
+
 interface PlayAgainProps {
-    modalOpen: boolean,
+  modalOpen: boolean,
 }
-*/
 
-export default function PlayAgain(/*props: PlayAgainProps*/ ): JSX.Element {
-  //const { modalOpen } = props;
 
-  const [open, setOpen] = useState(true);
+export default function PlayAgain(props: PlayAgainProps): JSX.Element {
+  const { modalOpen } = props;
+
+  const [open, setOpen] = useState(modalOpen);
+
+  const rewardRef = React.useRef<RewardElement>(null);
+
+  useEffect(() => {
+    setOpen(modalOpen);
+  }, [modalOpen]);
+
+  useEffect(() => {
+    if (open) {
+      setTimeout(() => {
+        rewardRef.current?.rewardMe();
+      }, 100);
+    }
+  }, [open]);
 
   return (
     <Modal
@@ -40,19 +55,24 @@ export default function PlayAgain(/*props: PlayAgainProps*/ ): JSX.Element {
       isOpen={open}
     >
       <div className="modal-content">
-        <h1>
-                    Congratulations!
-        </h1>
+        <Reward
+          ref={rewardRef}
+          type='confetti'
+        >
+          <h1>
+            Congratulations!
+          </h1>
+        </Reward>
 
         <p>
-                    You have successfully completed the lab! <br />
-                    Would you like to play again?
+          You have successfully completed the lab! <br />
+          Would you like to play again?
         </p>
 
         <ChoiceButton
           text="Play Again"
           toPage="/" //landing page, should take you here from last level
-          onClick={() => setOpen(false)}
+          onClick={() => rewardRef.current?.rewardMe()}
         />
       </div>
     </Modal>
