8af68ecd6cd4626bb2f884740f2b690e0ec8908b
Nathan Endow <35786272+nendow02@users.noreply.github.com>
2022-02-26 13:34:01 -0800

    Rewards (#87)
    * Added total button functionality
    
    * Added reward animation and moved logic to choiceButton component
    
    Co-authored-by: alvinh36 <alvinh36@gmail.com>

--- src/components/shared/ChoiceButton.tsx
+++ src/components/shared/ChoiceButton.tsx
@@ -1,15 +1,37 @@
+import React from 'react';
 import '../../styles/choiceButton.scss';
+import Reward, { RewardElement } from 'react-rewards';
 import { Link } from 'react-router-dom';
 interface statsProps {
   text: string;
   toPage: string;
-  onClick: (e: any) => void;
+  correctLogic: boolean;
+  correctAction: () => void;
 }
 
-export default function Stats(props:statsProps): JSX.Element {
+export default function Stats(props: statsProps): JSX.Element {
+  const rewardRef = React.useRef<RewardElement>(null);
   return (
-    <Link to={props.toPage}>
-      <button className="choice-button" onClick={props.onClick}>{props.text}</button>
-    </Link>
+    <Reward
+      ref={rewardRef}
+      type='confetti'
+      config={{
+        lifetime: 100,
+        angle: 80,
+      }}
+    >
+      <Link to={props.toPage}>
+        <button className="choice-button" onClick={(e) => {
+          if (props.correctLogic) {
+            props.correctAction();
+            rewardRef.current?.rewardMe();
+          } else {
+            e.preventDefault();
+            rewardRef.current?.punishMe();
+          }
+
+        }}>{props.text}</button>
+      </Link>
+    </Reward>
   );
 }
